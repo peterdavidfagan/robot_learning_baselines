0c3c203dc22ea13e5f8b90c3fda4b4b9ecd37e45
peterdavidfagan <peterdavidfagan@gmail.com>
2024-04-10 13:07:59 +0100

    add basic script to autmate inference tests

---
+++ robot_learning_baselines/scripts/benchmark_token_compression.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+poetry shell
+
+# config
+experiment_name="performance_benchmark"
+notes="performance benchmark"
+tags="performance benchmark" 
+parent_dir=$(realpath ..)
+cd ${parent_dir}
+
+# Run basic performance benchmarks
+python perf_bench_mutli_modal.py +config=octo-categorical config.wandb.experiment_name="octo_categorical_${experiment_name}"
+
+python perf_bench_mutli_modal.py +config=octo-categorical-compressed config.wandb.experiment_name="octo_categorical_${experiment_name}"
