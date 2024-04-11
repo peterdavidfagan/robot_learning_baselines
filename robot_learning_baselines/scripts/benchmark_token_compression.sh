d9c1ff60607ff0c69581f8e308619adaba16bcad
peterdavidfagan <peterdavidfagan@gmail.com>
2024-04-11 09:15:46 +0100

    add compressed to experiment name

--- robot_learning_baselines/scripts/benchmark_token_compression.sh
+++ robot_learning_baselines/scripts/benchmark_token_compression.sh
@@ -10,4 +10,4 @@ cd ${parent_dir}
 
 # Run basic performance benchmarks
 python perf_bench_mutli_modal.py +config=octo-categorical config.wandb.experiment_name="octo_categorical_${experiment_name}"
-python perf_bench_mutli_modal.py +config=octo-categorical-compressed config.wandb.experiment_name="octo_categorical_${experiment_name}"
+python perf_bench_mutli_modal.py +config=octo-categorical-compressed config.wandb.experiment_name="octo_categorical_compressed_${experiment_name}"
